3b17de45b15e857f3ab7d80ca162a3b4eb49a03c
MohamedShehataAbdalla <moh.shehata143@gmail.com>
2021-12-02 19:33:05 +0200

    Upload The Design

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* Start Carousel */
+
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 3000,
+  wrap: false
+});
+
+/* End Carousel */
